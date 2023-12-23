8c6e180e396531a859cb1be681e69a8226340888
GotameSafal <lamichhanem36@gmail.com>
2023-12-23 22:44:00 +0530

    added db uri

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
-   const {connection} =  await mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
+   const {connection} =  await mongoose.connect(process.env.MONGOB_URI);
    console.log(`successfully connected to database with host ${connection.host}`)
   } catch (error) {
     console.log(`can't connect to database`)
